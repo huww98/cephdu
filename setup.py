58a69ed276f64bd4003e672c0b8f87ae5bd32fb5
胡玮文 <huww98@outlook.com>
2021-04-06 00:44:45 +0800

    refine meta

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+import pathlib
+
+from setuptools import find_packages, setup
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='cephdu',
+    description='A "du" like utility to estimate file space usage on CephFS',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/huww98/cephdu',
     maintainer='Weiwen Hu',
     maintainer_email='huww98@outlook.com',
@@ -17,6 +26,7 @@ setup(
         'setuptools_scm',
     ],
     keywords=['ceph', 'cephfs', 'du'],
+    license='MIT',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
